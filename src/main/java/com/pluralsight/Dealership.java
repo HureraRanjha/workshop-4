15d477deb257b8463825050a7470f5d2bde8c475
HureraRanjha <hureraranjha9@gmail.com>
2026-05-08 12:37:19 -0400

    Phase 1 and 2 completed

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,93 @@
 package com.pluralsight;
 
-public class Dealership {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Dealership
+{
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone)
+    {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {return name; }
+    public void setName(String name) {this.name = name;}
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public Vehicle[] getVehiclesByPrice(int min, int max)
+    {
+        return null;
+    }
+
+    public Vehicle[] getVehiclesByMakeModel(String make, String model)
+    {
+        return null;
+    }
+
+    public Vehicle[] getVehiclesByYear(int min, int max)
+    {
+        return null;
+    }
+
+    public Vehicle[] getVehiclesByColor(String color)
+    {
+        return null;
+    }
+
+    public Vehicle[] getVehiclesByMileage(int min, int max)
+    {
+        return null;
+    }
+
+    public Vehicle[] getVehiclesByType(String vehicleType)
+    {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles()
+    {
+        ArrayList<Vehicle> inventoryCopy = new ArrayList<>();
+        Collections.copy(inventory, inventoryCopy);
+        return inventoryCopy;
+    }
+
+    public void addVehicle(Vehicle vehicle)
+    {
+        inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle)
+    {
+        inventory.remove(vehicle);
+    }
+
 }
--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,63 @@
 package com.pluralsight;
 
-public class DealershipFileManager {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DealershipFileManager
+{
+    String INVENTORY_PATH = "src/main/resources/inventory.csv";
+
+    public Dealership getDealership()
+    {
+        String name = "";
+        String address = "";
+        String phone = "";
+        Dealership dealership = null;
+
+        try
+        {
+            FileReader fileReader = new FileReader(INVENTORY_PATH);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String[] dealerShipHeader = bufferedReader.readLine().split("\\|");
+
+            name = dealerShipHeader[0];
+            address = dealerShipHeader[1];
+            phone = dealerShipHeader[2];
+
+            dealership = new Dealership(name, address, phone);
+
+            String input = "";
+            while((input = bufferedReader.readLine()) != null)
+            {
+                String[] parsedVehicle = input.split("\\|");
+                int vin = Integer.parseInt(parsedVehicle[0]);
+                int year = Integer.parseInt(parsedVehicle[1]);
+                String make = parsedVehicle[2];
+                String model = parsedVehicle[3];
+                String vehicleType = parsedVehicle[4];
+                String color = parsedVehicle[5];
+                int odometer = Integer.parseInt(parsedVehicle[6]);
+                double price = Double.parseDouble(parsedVehicle[7]);
+
+                dealership.addVehicle(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.err.println("Something wrong reading the file");
+        }
+        catch(IOException e)
+        {
+            System.err.println("Cannot open the file");
+        }
+        return dealership;
+    }
+
+    public void saveDealership(Dealership dealership)
+    {
+
+    }
 }
